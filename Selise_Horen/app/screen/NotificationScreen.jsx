1275825267c16abbbf843b2d83ddb921bb1b4dd7
Sanjida Akter <70808876+San-shor@users.noreply.github.com>
2023-05-09 09:15:06 +0600

    Delete Selise_Horen directory

--- Selise_Horen/app/screen/NotificationScreen.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-
-const NotificationScreen = () => {
-  return <View></View>;
-};
-
-export default NotificationScreen;
